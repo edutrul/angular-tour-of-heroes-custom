3026ba4f4b402359c271d1a1fcc22ab79a2de506
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2018-05-01 17:45:21 -0500

    Add click element, if, for.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,16 +9,15 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Omniknight',
-    machine_name: 'omniknight',
-    weapons: ['knife', 'wand']
-  };
-
   heroes = HEROES;
 
+  selectedHero: Hero;
+
   constructor() { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   ngOnInit() {
   }
