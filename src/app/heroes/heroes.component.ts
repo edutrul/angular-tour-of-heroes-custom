a275a74ac7794625ea9925f712e9eff0c1c73dcd
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2018-05-01 23:00:01 -0500

    Add service to inject heroes and make it observable to be asynchronous call.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,17 +9,25 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  // First time call to create object(According to life cycle of angular.)
+  // @see: https://angular.io/guide/lifecycle-hooks.
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
